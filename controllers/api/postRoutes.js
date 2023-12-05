cdb3e568d25a18f97d3cc6dc1e45ddcf8c0626d0
joshhigg <136753578+joshhigg@users.noreply.github.com>
2023-12-05 14:26:45 -0700

    added commentability and edit/delete

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -18,6 +18,47 @@ router.post('/', withAuth, async (req, res) => {
     }
 });
 
+// View one post
+router.get('/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        if (!postData) {
+            return res.status(404).json({ message: 'Post not found' });
+        }
+
+        const post = postData.get({ plain: true });
+
+        return res.json(post);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Update post
+router.put('/:id', async (req, res) => {
+    const updatedPost = await Post.update(
+        {
+            title: req.body.title,
+            content: req.body.content,
+        },
+        {
+            where: {
+                id: req.params.id,
+            },
+        }
+    );
+
+    res.json(updatedPost);
+});
 
 // Delete post
 router.delete('/:id', withAuth, async (req, res) => {
